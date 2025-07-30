fb7a78c9f05493b2b9b8fdd45b518d0cc2fb38db
sofiaanunez <sofianunezz99@gmail.com>
2025-07-30 00:09:27 -0300

    cambios segunda entrega

--- js/catalogo.js
+++ js/catalogo.js
@@ -3,24 +3,28 @@
         id:1,
         especie: "Rosas",
         color:"Rojas",
-        precio:4000  
+        precio:70000,
+        cantidad:0
     },
     {
         id:2,
         especie: "Jazmines",
         color:"Blancos",
-        precio:3000
+        precio:60000,
+        cantidad:0
     },
     {
         id:3,
-        especie: "Orquideas",
-        color:"Violetas",
-        precio:6000
+        especie: "Girasoles",
+        color:"",
+        precio:50000,
+        cantidad:0
     },
     {
         id:4, 
         especie: "Fresias",
         color:"Multicolor", 
-        precio:500
+        precio:20000,
+        cantidad:0
     }
     ];
--- js/floreria.js
+++ js/floreria.js
@@ -1,15 +1,16 @@
     const section = document.querySelector("#seccion-flores");
-    const itemsCarrito = document.getElementById("items-carrito");
-    const totalCarrito = document.getElementById("total-carrito");
-    const toggleButton = document.getElementById("toggle-carrito");
-    const carritoContenedor = document.getElementById("carrito");
+    const carritoCompras = document.querySelector("#carrito");
+    const buttonCarrito = document.querySelector("#toggle-carrito");
+    const items = document.querySelector("#items");
+    const cantidadItems = document.querySelector("#cantidad");
+    const total = document.querySelector("#total")
 
     const flores = productos.map(
         (producto) =>
             `<div class="card" style="width:300px">
-                <img src="./images/logo.png" class="card-img-top" alt="${producto.especie}"/>
+                <img src="./images/${producto.id}.jpg" class="card-img-top" alt="${producto.especie}"/>
                 <div class="card-body">
-                    <h5 class="card-title">${producto.especie} - ${producto.color}</h5>
+                    <h4 class="card-title">${producto.especie} ${producto.color}</h4>
                     <p class="card-text">$${producto.precio}</p>
                     <button class="agregar-carrito" data-id="${producto.id}">Agregar al carrito</button>
                 </div>
@@ -17,46 +18,69 @@
     );
 
     section.innerHTML = flores.join("");
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
 
-    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    if (!Array.isArray(carrito)) {
+        carrito = [];
+    }
 
-    toggleButton.addEventListener("click", () => {
-        carritoContenedor.classList.toggle("mostrar");
+    buttonCarrito.addEventListener("click", () => {
+        carritoCompras.classList.toggle("mostrar");
     });
 
     function actualizarCarrito() {
-        itemsCarrito.innerHTML = "";
-        let total = 0;
+        // info carrito de compras
+        items.innerHTML = "";
+        let precioTotal = 0;
 
-        carrito.forEach((producto, index) => {
+        carrito.forEach((producto) => {
             const div = document.createElement("div");
             div.innerHTML = `
-                <span>${producto.especie} - $${producto.precio}</span>
-                <button class="btn-eliminar-carrito" data-index="${index}">🗑️</button>
+                <span>${producto.especie} - ${producto.cantidad}</span>
+                <button class="button-eliminar-carrito" data-id="${producto.id}">🗑️</button>
             `;
-            itemsCarrito.appendChild(div);
-            total += producto.precio;
+            items.appendChild(div);
+            precioTotal += producto.precio * producto.cantidad;
         });
 
-        totalCarrito.textContent = `Total: $${total}`;
-        document.getElementById("cantidad").textContent = carrito.length;
+        total.textContent = `total: $${precioTotal}`;
+        cantidadItems.textContent = carrito.length;
     }
 
-    document.addEventListener("click", function(event){
+    document.addEventListener("click", (event) =>{
         if(event.target.classList.contains("agregar-carrito")){
             const id = parseInt(event.target.dataset.id);
-            const producto = productos.find(prod => prod.id === id);
-            carrito.push(producto);
+            const productoSeleccionado = productos.find(prod => prod.id == id);
+            console.log(productoSeleccionado);
+            const productoExistente = carrito.find(item => item.id === productoSeleccionado.id);
+            if(productoExistente){
+                productoExistente.cantidad +=1;
+            }else{
+                //si es nuevo, crea nuevo item (objeto) y se agrega a mi array de carrito
+                carrito.push({...productoSeleccionado, cantidad:1});
+            }
             localStorage.setItem("carrito", JSON.stringify(carrito));
             actualizarCarrito();
         }
 
-        if (event.target.classList.contains("btn-eliminar-carrito")) {
-            const index = parseInt(event.target.dataset.index);
-            carrito.splice(index, 1);
+        if(event.target.classList.contains("button-eliminar-carrito")){
+            // obtengo id del elemento que seleccioné en el html
+            const itemABorrar = parseInt(event.target.dataset.id);
+            // busco indice dentro de mi array carrito en base al id del producto seleccionado
+            const index = carrito.findIndex(item => item.id === itemABorrar);
+
+            if(carrito[index].cantidad > 1){
+                carrito[index].cantidad -= 1;
+            }else{
+                carrito.splice(index,1);
+            }
+            
+            //actualizo el localStorage
             localStorage.setItem("carrito", JSON.stringify(carrito));
             actualizarCarrito();
         }
+
     });
 
+    // para que el carrito siempre muestre cantidadItems si no se borró el almacenamiento
     actualizarCarrito();
